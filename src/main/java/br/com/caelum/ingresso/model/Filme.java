1c2493ee367559ab3e0e57d0f19192abf3ca0e25
bdmoriki <dupim1@hotmail.com>
2017-05-17 20:55:45 -0300

    teste'

--- src/main/java/br/com/caelum/ingresso/model/Filme.java
+++ src/main/java/br/com/caelum/ingresso/model/Filme.java
@@ -3,6 +3,8 @@ package br.com.caelum.ingresso.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import java.math.BigDecimal;
 import java.time.Duration;
 
 /**
@@ -14,10 +16,30 @@ public class Filme {
     @Id
     @GeneratedValue
     private Integer id;
+    
     private String nome;
+    
     private Duration duracao;
+    
     private String genero;
-    public Integer getId() {
+    
+    private BigDecimal preco;
+    
+    /**
+     * @deprecated hibernate only
+     */
+    public Filme() {
+
+    }
+    
+    public Filme(String nome, Duration duracao, String genero, BigDecimal preco) {
+		this.nome = nome;
+		this.duracao = duracao;
+		this.genero = genero;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
         return id;
     }
 
@@ -48,4 +70,8 @@ public class Filme {
     public void setGenero(String genero) {
         this.genero = genero;
     }
+    
+    public BigDecimal getPreco() {
+		return preco;
+	}
 }
